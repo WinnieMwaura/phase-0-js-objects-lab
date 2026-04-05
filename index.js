0f55a100911d0a9209f6ceaace3d801b03a17630
WinnieMwaura <Winniemwaura198@gmail.com>
2026-04-05 14:54:19 +0300

    objectschanges

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-//Write your code here
+// Create attendee object
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
 
+// Function to log attendee name
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
 
+// Function to log ticket price
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
 
-//Needed for the tests to work. Don't modify
+// Function to update ticket type
+function updateTicketType(attendee, newTicketType) {
+  attendee.ticketType = newTicketType;
+}
+
+// Function to update ticket price
+function updateTicketPrice(attendee, newTicketPrice) {
+  attendee.ticketPrice = newTicketPrice;
+}
+
+// Function to remove event property
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+// Function to add checkedIn property
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
+
+// Export (your provided code will pick these up)
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
